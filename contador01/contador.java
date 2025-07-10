4c3b4051c434befeb414c3af486353dfc39b043f
Carlos Eduardo Ferreira <karloseduardoferreira2@gmail.com>
2025-07-10 02:31:54 -0300

    Add files via upload
    1. **contador.java**  
       - Implementa um laço `while` simples que conta de 0 até 9, exibindo o número atual da iteração.  
       - Demonstra a estrutura básica de repetição com incremento.
    
    2. **contadorbreak.java**  
       - Utiliza um laço `while` para contar até 10, porém com uma condição de interrupção: ao alcançar o valor 5, o comando `break` é acionado, interrompendo o loop antecipadamente.  
       - Mostra como o `break` pode ser usado para sair de um loop baseado em condições específicas.
    
    3. **contadorcontinue.java**  
       - Executa um laço `while` de 0 a 9, porém utiliza o comando `continue` para pular as iterações em que o contador é par (números divisíveis por 2).  
       - Demonstra o uso do `continue` para pular partes específicas da iteração mantendo o loop ativo.
    
    Esses exemplos são fundamentais para entender o controle de fluxo em Java, especialmente em cenários onde é necessário manipular como e quando o loop deve continuar, interromper ou pular ciclos.  
    Compreender essas estruturas é essencial para a criação de algoritmos eficientes e claros.

---
+++ contador01/contador.java
@@ -0,0 +1,14 @@
+package contador01;
+
+public class contador {
+    public static void main(String[] args) {
+        
+        int c  = 0;
+        while(c < 10){
+            System.out.println("Loop " + c);
+            c++;
+        }
+
+    }
+
+}
