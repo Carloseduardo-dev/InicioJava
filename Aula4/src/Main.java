fbd7937f9d80b3739be877d44bc17c2b7d6e69b4
Carlos Eduardo S Ferreira <karloseduardoferreira2@gmail.com>
2025-07-18 22:08:52 -0300

    Update Main.java

--- Aula4/src/Main.java
+++ Aula4/src/Main.java
@@ -1,14 +1,24 @@
 public class Main {
     public static void main(String[] args) {
+        Caneta canete1 = new Caneta();
+        canete1.setModelo("ABC");
+        canete1.setCor("verde");
+        canete1.status();
+        canete1.rabiscar();
 
-    Caneta caneta1 = new Caneta("Preta", "NIC", 0.4f);
+        //método construtor
+    /*Caneta caneta1 = new Caneta("Preta", "NIC", 0.4f);
     caneta1.status();
-    caneta1.rabiscar();
+    caneta1.rabiscar();*/
 
-   /* var caneta2 = new Caneta();
+   var caneta2 = new Caneta();
+    caneta2.setModelo("CDF");
+    caneta2.setCor("preta");
+    caneta2.setPonta(0.4f);
+    caneta2.setCarga(80);
     caneta2.setTampada(false);
     caneta2.status();
-    caneta2.rabiscar();*/
+    caneta2.rabiscar();
 
 
     }
